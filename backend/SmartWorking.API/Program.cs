dce8a783423b504e33cee2e18ea38816ba29fa68
Paolo Bini <bini.fos@gmail.com>
2026-02-25 21:35:18 +0100

    fix swagger

--- backend/SmartWorking.API/Program.cs
+++ backend/SmartWorking.API/Program.cs
@@ -67,11 +67,8 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
